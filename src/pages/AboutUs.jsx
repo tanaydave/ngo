511ff802b833b87edaba9e3a7390b48dc8df864e
Tanay <daveth1106@gmail.com>
2023-07-02 14:48:30 +0530

    initial commit (after restoration)

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,6 @@
 import {React } from "react";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../components/navbar"
+import Footer from "../components/footer"
 import { useState } from "react";
 import mission from '../assets/mission.jpg'
 import vision from '../assets/vision.jpg'
